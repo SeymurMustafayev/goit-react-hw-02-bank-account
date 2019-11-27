a64ad9a07c3da8b3396e139226833045c604d923
Seymur <mustafav.seymur@gmail.com>
2019-11-27 14:09:10 +0200

    Updates

---
+++ precache-manifest.1f8391fc10a7899a66e3f11c2599b6f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a582152d778c9fba94d21af6219aa81d",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "01b9f1be4e57285cc260",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "1b0bb700baccedd3c550",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.bd2e86fe.chunk.css"
+  },
+  {
+    "revision": "01b9f1be4e57285cc260",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.985dae6b.chunk.js"
+  },
+  {
+    "revision": "1b0bb700baccedd3c550",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.70226c75.chunk.js"
+  },
+  {
+    "revision": "34b01132a4e3c4561d64",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.c5443573.js"
+  }
+]);
